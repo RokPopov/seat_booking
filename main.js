1f0ea61288781ff11b739a2db5c8148f234f71c8
rokpopov <rokpopov90@gmail.com>
2021-11-23 08:18:42 +0100

    save the index of the selected seats, index of selected movie and ticket price in local storage

--- main.js
+++ main.js
@@ -7,10 +7,21 @@ const movieSelect = document.getElementById("movie");
 // changing the type of the movieSelect into a number
 let ticketPrice = +movieSelect.value;
 
+// save selected movide index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 // update the total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const selectedSeatsCount = selectedSeats.length;
+  // copy selected seats into array, map through array, return a new array indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  // put the array in local storage
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+  // retrieve the array from local local storage
 
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -19,6 +30,7 @@ function updateSelectedCount() {
 // movie-select event listener
 movieSelect.addEventListener("change", function (e) {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
